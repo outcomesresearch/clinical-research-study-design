84524577a23eafa53aa0738779a579820cef4e3e
jpiccirillo <piccirilloj1@gmail.com>
2024-06-23 18:15:28 -0500

    Update vue config and README

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
-# vue-decision-tree
+# clinical-research-study-design
 
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+Online step-based wizard tool to help clinical study investigators select the correct study design for their circumstances.
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,24 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
 
-  pluginOptions: {
-    vuetify: {
-			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
-  }
-})
+
+const webpackParams = {
+  title: 'Choose the Correct Study Design',
+  description:
+    'Online step-based wizard tool to help clinical study investigators select the correct study design for their circumstances.',
+  site_name:
+    'Applets from Clinical Outcomes Research at Washington University in St Louis',
+  url: 'https://outcomesresearch.github.io/clinical-research-study-design/',
+};
+
+module.exports = {
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/clinical-research-study-design/' : '/',
+  transpileDependencies: ['vuetify'],
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      Object.entries(webpackParams).forEach(([key, value]) => {
+        args[0][key] = value;
+      });
+      return args;
+    });
+  },
+};
